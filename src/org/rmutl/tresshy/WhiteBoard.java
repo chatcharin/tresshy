614fd7b8c12f8471749903c08ccb3f5ad1d17fff
bamboor <bamboor@1c0080cb-f852-0410-8840-11315fbf0098>
2008-12-29 01:34:21 +0000

    update source whiteboard

--- src/org/rmutl/tresshy/WhiteBoard.java
+++ src/org/rmutl/tresshy/WhiteBoard.java
@@ -5,24 +5,22 @@
 
 package org.rmutl.tresshy;
 
-import java.io.IOException;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.geom.Point2D;
 import java.net.MalformedURLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.media.jai.PerspectiveTransform;
 import org.rmutl.tresshy.calibration.ScreenFrame;
 import org.rmutl.tresshy.mouse.RobotMouse;
-import wiiremotej.IRLight;
-import wiiremotej.WiiRemote;
-import wiiremotej.WiiRemoteJ;
-import wiiremotej.event.WRIREvent;
-import wiiremotej.event.WRStatusEvent;
 
 /**
  *
  * @author catchajaa
  */
 public class WhiteBoard {
+   private MainUI   mainui;
    private  WiiMote wiimote;
    private  WebCam  webcam;
    private  int     EMTY   = 0;
@@ -52,14 +50,40 @@ public class WhiteBoard {
    private int j;
    private static PerspectiveTransform perspective;
    private RobotMouse mouse;
-   public WhiteBoard(){
+   public WhiteBoard(MainUI mainui){
         i = 0;
         j = 1;
-        screensize = "1280 x 800";
-        calibrat = true;
-        sleep = true;
-        mouse = new RobotMouse();
+        this.mainui = mainui;
+        screensize  = "1280 x 800";
+        WiiMote.calibrat    = true;
+        sleep       = true;
+        mouse       = new RobotMouse();
     }
+
+	static PerspectiveTransform pt;
+
+	public static void setTransform(double[] src)
+	{
+		Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+
+//		the destination quad is the screen. Because the calibration-points are not the real vertices of the
+//		screen, the destination quad is smaller than the screen
+		double[] dest = {90,90,screen.width-90,90,screen.width-90,screen.height-90,90,screen.height-90};
+		pt = PerspectiveTransform.getQuadToQuad(
+				src[0],src[1],src[2],src[3],src[4],src[5],src[6],src[7],
+				dest[0],dest[1],dest[2],dest[3],dest[4],dest[5],dest[6],dest[7]);
+	}
+
+//	transforms the given point
+	public static Point2D getTransformedPoint(Point2D pp)
+	{
+		if(pt!=null){
+			Point2D p =  pt.transform(pp,null);
+			return p;
+		} else {
+			return null;
+		}
+	}
     public void startCalibration(){
          try {
             screen = new ScreenFrame();
@@ -70,13 +94,16 @@ public class WhiteBoard {
     public void  calibration(double x,double y,double size){
         if(index < 30 && (index != 3)){
             index++;
-        }else if(calibrat && (index==3)){
+        }else if(WiiMote.calibrat && (index==3)){
           screen.setPoint(i,j,x,y);
-          calibrat = screen.nextState();
+          WiiMote.calibrat = screen.nextState();
           screen.refresh();
-          if(!calibrat)
+          if(!WiiMote.calibrat)
           {
             perspective = screen.calculateTransformation();
+          //  screen.clean();
+          //  screen.setEnabled(false);
+            screen.dispose();
             screen.setVisible(false);
           }
           i+=2;
@@ -90,7 +117,7 @@ public class WhiteBoard {
     }
 
     public void  action(double x,double y,double size){
-        if(!calibrat)
+        if(!WiiMote.calibrat)
            mouse.mouseClick(perspective.transform(new IRDot(1,x,y,size),null));
     }
 
@@ -119,7 +146,7 @@ public class WhiteBoard {
     public void connect(){
         switch(select){
               case  0:wiimote.connection();break;
-              case  1:webcam.connection();break;
+              case  1:mainui.WebCam();break;
               case  2:wiimote.connection();break;
               default:wiimote.connection();break;
         }
@@ -131,7 +158,7 @@ public class WhiteBoard {
    public void calibration(){
        switch(select){
               case  0:wiimote.calibration();break;
-              case  1:webcam.calibration();break;
+              case  1:mainui.calibratWebCam();break;
               case  2:wiimote.calibration();break;
               default:wiimote.calibration();break;
         }
@@ -139,9 +166,12 @@ public class WhiteBoard {
    public void showMonitor() {
            switch(select){
               case  0:wiimote.showMonitor();break;
-              case  1:webcam.showMonitor();break;
+              case  1:mainui.showWebCam();break;
               case  2:wiimote.showMonitor();break;
               default:wiimote.showMonitor();break;
         }
     }
+   public boolean isConnect(){
+       return  wiimote.isConnect();
+   }
 }
