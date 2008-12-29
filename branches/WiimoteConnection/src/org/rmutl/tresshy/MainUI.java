b392b09dc96d6b8e170f8a61f48e2fa2dd7cd718
bamboor <bamboor@1c0080cb-f852-0410-8840-11315fbf0098>
2008-12-29 01:32:47 +0000

    update source whiteboard

--- branches/WiimoteConnection/src/org/rmutl/tresshy/MainUI.java
+++ branches/WiimoteConnection/src/org/rmutl/tresshy/MainUI.java
@@ -11,16 +11,50 @@
 
 package org.rmutl.tresshy;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.media.CannotRealizeException;
+import javax.media.NoPlayerException;
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JSlider;
+import javax.swing.JToggleButton;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.plaf.metal.MetalLookAndFeel;
+import org.rmutl.tresshy.camera.CalibrationFrame;
+import org.rmutl.tresshy.camera.IRDotPanel;
+import org.rmutl.tresshy.camera.MouseControl;
+import org.rmutl.tresshy.camera.VideoDisplay;
+import org.rmutl.tresshy.camera.calibrateWebcam;
+
 /**
  *
  * @author catchajaa
  */
 public class MainUI extends javax.swing.JFrame {
+    private static MainUI mainui;
     private WhiteBoard whiteboard;
+    private boolean connect;
+    private boolean webcamconnect;
     /** Creates new form MainUI */
     public MainUI() {
         initComponents();
-        whiteboard = new WhiteBoard();
+        connect = false;
+        whiteboard = new WhiteBoard(this);
     }
 
     /** This method is called from within the constructor to
@@ -49,13 +83,19 @@ public class MainUI extends javax.swing.JFrame {
         jLabel9 = new javax.swing.JLabel();
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenu1 = new javax.swing.JMenu();
+        jMenuItem3 = new javax.swing.JMenuItem();
         jMenu2 = new javax.swing.JMenu();
+        jMenuItem2 = new javax.swing.JMenuItem();
         jMenu3 = new javax.swing.JMenu();
         jMenuItem1 = new javax.swing.JMenuItem();
 
         jLabel4.setText("jLabel4");
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        addWindowListener(new java.awt.event.WindowAdapter() {
+            public void windowClosed(java.awt.event.WindowEvent evt) {
+                mainClose(evt);
+            }
+        });
 
         jPanel1.setBorder(javax.swing.BorderFactory.createTitledBorder("Setting"));
 
@@ -67,6 +107,11 @@ public class MainUI extends javax.swing.JFrame {
         });
 
         jButton2.setText("Calibration");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
 
         buttonConnect.setText("Connection");
         buttonConnect.addActionListener(new java.awt.event.ActionListener() {
@@ -82,9 +127,9 @@ public class MainUI extends javax.swing.JFrame {
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(buttonConnect, javax.swing.GroupLayout.DEFAULT_SIZE, 112, Short.MAX_VALUE)
-                    .addComponent(buttonMonitor, javax.swing.GroupLayout.DEFAULT_SIZE, 112, Short.MAX_VALUE)
-                    .addComponent(jButton2, javax.swing.GroupLayout.DEFAULT_SIZE, 112, Short.MAX_VALUE))
+                    .addComponent(buttonConnect, javax.swing.GroupLayout.DEFAULT_SIZE, 109, Short.MAX_VALUE)
+                    .addComponent(buttonMonitor, javax.swing.GroupLayout.DEFAULT_SIZE, 109, Short.MAX_VALUE)
+                    .addComponent(jButton2, javax.swing.GroupLayout.DEFAULT_SIZE, 109, Short.MAX_VALUE))
                 .addContainerGap())
         );
         jPanel1Layout.setVerticalGroup(
@@ -114,7 +159,7 @@ public class MainUI extends javax.swing.JFrame {
                 .addContainerGap()
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jLabel1)
-                    .addComponent(jProgressBar1, javax.swing.GroupLayout.DEFAULT_SIZE, 112, Short.MAX_VALUE)
+                    .addComponent(jProgressBar1, javax.swing.GroupLayout.DEFAULT_SIZE, 109, Short.MAX_VALUE)
                     .addComponent(jLabel2)
                     .addComponent(screensize))
                 .addContainerGap())
@@ -135,6 +180,11 @@ public class MainUI extends javax.swing.JFrame {
         jPanel3.setBorder(javax.swing.BorderFactory.createTitledBorder("Mouse Setting"));
 
         jCheckBox1.setText("Smooth");
+        jCheckBox1.addChangeListener(new javax.swing.event.ChangeListener() {
+            public void stateChanged(javax.swing.event.ChangeEvent evt) {
+                jCheckBox1StateChanged(evt);
+            }
+        });
 
         jCheckBox2.setText("ClickRigth");
 
@@ -147,7 +197,7 @@ public class MainUI extends javax.swing.JFrame {
                 .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jCheckBox1)
                     .addComponent(jCheckBox2))
-                .addContainerGap(41, Short.MAX_VALUE))
+                .addContainerGap(38, Short.MAX_VALUE))
         );
         jPanel3Layout.setVerticalGroup(
             jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -159,14 +209,37 @@ public class MainUI extends javax.swing.JFrame {
                 .addContainerGap(17, Short.MAX_VALUE))
         );
 
-        selectdevice.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "WiiMote", "WebCam", " " }));
+        selectdevice.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "WiiMote", "WebCam" }));
+        selectdevice.addItemListener(new java.awt.event.ItemListener() {
+            public void itemStateChanged(java.awt.event.ItemEvent evt) {
+                selectdeviceItemStateChanged(evt);
+            }
+        });
 
         jLabel9.setText("Select Device:");
 
         jMenu1.setText("File");
+
+        jMenuItem3.setText("Close");
+        jMenuItem3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem3ActionPerformed(evt);
+            }
+        });
+        jMenu1.add(jMenuItem3);
+
         jMenuBar1.add(jMenu1);
 
         jMenu2.setText("Edit");
+
+        jMenuItem2.setText("WebCamSetting");
+        jMenuItem2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem2ActionPerformed(evt);
+            }
+        });
+        jMenu2.add(jMenuItem2);
+
         jMenuBar1.add(jMenu2);
 
         jMenu3.setText("Help?");
@@ -186,10 +259,10 @@ public class MainUI extends javax.swing.JFrame {
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jPanel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                    .addGroup(layout.createSequentialGroup()
                         .addComponent(jLabel9)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(selectdevice, javax.swing.GroupLayout.PREFERRED_SIZE, 76, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(selectdevice, 0, 67, Short.MAX_VALUE))
                     .addComponent(jPanel2, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addComponent(jPanel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addContainerGap())
@@ -214,26 +287,304 @@ public class MainUI extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void buttonMonitorActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_buttonMonitorActionPerformed
-        // TODO add your handling code here:
+        whiteboard.showMonitor();
 }//GEN-LAST:event_buttonMonitorActionPerformed
 
     private void buttonConnectActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_buttonConnectActionPerformed
+     if(!connect){
       whiteboard.select(selectdevice.getSelectedIndex());
+      System.out.println("SelectItem"+selectdevice.getSelectedIndex());
+      whiteboard.run();
       whiteboard.connect();
+      connect = true;
       screensize.setText("Display Size :"+whiteboard.getScreenSize());
+     }
 }//GEN-LAST:event_buttonConnectActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        whiteboard.calibration();
+    }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void selectdeviceItemStateChanged(java.awt.event.ItemEvent evt) {//GEN-FIRST:event_selectdeviceItemStateChanged
+        if(connect){
+            whiteboard.disconnect();
+            connect = false;
+        }
+    }//GEN-LAST:event_selectdeviceItemStateChanged
+
+    private void jMenuItem2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem2ActionPerformed
+        // TODO add your handling code here:
+        if(webcamconnect)
+          webcamSetting();
+    }//GEN-LAST:event_jMenuItem2ActionPerformed
+
+    private void mainClose(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_mainClose
+         if(connect){
+            whiteboard.disconnect();
+            connect = false;
+        }
+         while(true){
+             if(!whiteboard.isConnect()){
+                  mainui.setVisible(false);
+                  System.exit(0);
+             }
+         }
+    }//GEN-LAST:event_mainClose
+
+    private void jCheckBox1StateChanged(javax.swing.event.ChangeEvent evt) {//GEN-FIRST:event_jCheckBox1StateChanged
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jCheckBox1StateChanged
+
+    private void jMenuItem3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem3ActionPerformed
+       if(connect){
+            whiteboard.disconnect();
+            connect = false;
+        }
+         while(true){
+             if(!whiteboard.isConnect()){
+                  mainui.setVisible(false);
+                  System.exit(0);
+             }
+         }
+    }//GEN-LAST:event_jMenuItem3ActionPerformed
 
     /**
     * @param args the command line arguments
     */
     public static void main(String args[]) {
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new MainUI().setVisible(true);
+
+
+            mainui =     new MainUI();
+            mainui.setVisible(true);
+            try
+               {
+            try {
+                // Con questa imposto il tema
+                UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+                SwingUtilities.updateComponentTreeUI(mainui);
+            } catch (ClassNotFoundException ex) {
+                Logger.getLogger(MainUI.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (InstantiationException ex) {
+                Logger.getLogger(MainUI.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (IllegalAccessException ex) {
+                Logger.getLogger(MainUI.class.getName()).log(Level.SEVERE, null, ex);
             }
-        });
+        }
+        catch(UnsupportedLookAndFeelException e)
+        {
+    	e.printStackTrace();
+      }
     }
 
+
+	static Toolkit t;
+	private static final long serialVersionUID = 1L;
+	public static VideoDisplay vp = new VideoDisplay();
+	public static IRDotPanel ip = new IRDotPanel();
+	public static boolean init = false;
+	public static JLabel status;
+	public static JToggleButton cal;
+	JButton cam;
+	JButton dot;
+
+	public static Thread mc;
+	public static CalibrationFrame cf;
+	public static JSlider irsens;
+
+
+
+	public void WebCam(){
+
+        webcamconnect = true;
+		JPanel buttons = new JPanel();
+		JPanel bottom = new JPanel();
+
+		irsens = new JSlider();
+		irsens.setPaintTicks(true);
+		irsens.setMajorTickSpacing(20);
+		irsens.setPaintLabels(true);
+		irsens.setValue(70);
+		irsens.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(),"IR-Sensivity"));
+
+		cal = new JToggleButton("start calibration");
+		cam = new JButton("Cam Monitor");
+		dot = new JButton("IRDot Monitor");
+		buttons.add(cal);
+		buttons.add(cam);
+		buttons.add(dot);
+		buttons.setBorder(BorderFactory.createEtchedBorder());
+
+		status = new JLabel();
+
+		bottom.add(status);
+
+
+		add(buttons, BorderLayout.NORTH);
+		add(bottom, BorderLayout.SOUTH);
+
+		t = Toolkit.getDefaultToolkit();
+		JFrame d = new JFrame();
+		d.setResizable(false);
+		d.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		JLabel l = new JLabel("Connecting to capture device...");
+		d.setTitle("Status");
+		JPanel dp = new JPanel();
+		dp.add(l);
+		d.add(dp);
+		d.pack();
+		d.setVisible(true);
+		d.setLocation((int)(t.getScreenSize().getWidth()-d.getWidth())/2,(int)(t.getScreenSize().getHeight()-d.getHeight())/2);
+		connectToWebcam();
+		d.dispose();
+
+
+		cal.addActionListener(new ActionListener(){
+
+			@SuppressWarnings("deprecation")
+			public void actionPerformed(ActionEvent arg0) {
+				if(cal.getText() == "start calibration")
+				{
+					cf = new CalibrationFrame();
+
+
+					cal.setText("reset");
+					calibrateWebcam.setTouchScreenVertices();
+					mc = new Thread(new MouseControl());
+					mc.start();
+				} else {
+					status.setText("Ready for calibration.");
+					mc.stop();
+					calibrateWebcam.resetCalibration();
+					cal.setText("start calibration");
+				}
+
+
+
+			}
+
+		});
+		cam.addActionListener(new ActionListener(){
+
+			public void actionPerformed(ActionEvent arg0) {
+
+					JFrame camd = new JFrame();
+					Dimension d = new Dimension(300,260);
+					vp.setPreferredSize(d);
+					vp.setSize(d);
+					vp.setBorder(BorderFactory.createEtchedBorder());
+					camd.add(vp);
+					camd.setSize(350,310);
+					camd.setLocation(100,220);
+					camd.setVisible(true);
+					camd.setTitle("Cam Monitor");
+					camd.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+					camd.setAlwaysOnTop(true);
+
+
+			}
+
+		});
+		dot.addActionListener(new ActionListener(){
+
+			public void actionPerformed(ActionEvent arg0) {
+				JFrame camd = new JFrame();
+				Dimension d = new Dimension(300,260);
+
+				ip.setPreferredSize(d);
+				ip.setSize(d);
+				ip.setBorder(BorderFactory.createEtchedBorder());
+				ip.setBackground(Color.BLACK);
+				JPanel camPanel = new JPanel();
+				camPanel.add(ip, BorderLayout.NORTH);
+				camPanel.add(irsens, BorderLayout.SOUTH);
+				camd.add(camPanel);
+//				camd.add(ip);
+//				camd.add(irsens);
+				camd.setSize(350,380);
+//				camd.pack();
+				camd.setLocation(100,200);
+				camd.setVisible(true);
+				camd.setTitle("IRDot Monitor");
+				camd.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+				camd.setAlwaysOnTop(true);
+			}
+
+		});
+
+
+
+
+	}
+
+
+
+	public void connectToWebcam()
+	{
+
+		try {
+			init = vp.initialise();
+			if(init == true)
+				{
+//					wiicam.setEnabled(false);
+					status.setText("Connected to capture device! Ready for calibration.");
+					setVisible(true);
+				} else {
+					System.exit(0);
+				}
+		} catch (NoPlayerException e) {
+			JOptionPane.showMessageDialog(null, "Could not connect to capture device!");
+			System.exit(0);
+		} catch (CannotRealizeException e) {
+			JOptionPane.showMessageDialog(null, "Could not connect to capture device!");
+			System.exit(0);
+		} catch (IOException e) {
+			JOptionPane.showMessageDialog(null, "Could not connect to capture device!");
+			System.exit(0);
+		}
+	}
+    public void showWebCam(){
+        JFrame camd = new JFrame();
+					Dimension d = new Dimension(300,260);
+					vp.setPreferredSize(d);
+					vp.setSize(d);
+					vp.setBorder(BorderFactory.createEtchedBorder());
+					camd.add(vp);
+					camd.setSize(350,310);
+					camd.setLocation(100,220);
+					camd.setVisible(true);
+					camd.setTitle("Cam Monitor");
+					camd.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+					camd.setAlwaysOnTop(true);
+    }
+    public void calibratWebCam(){
+                    cf = new CalibrationFrame();
+					cal.setText("reset");
+					calibrateWebcam.setTouchScreenVertices();
+					mc = new Thread(new MouseControl());
+					mc.start();
+    }
+   public void webcamSetting(){
+            	JFrame camd = new JFrame();
+				Dimension d = new Dimension(300,260);
+
+				ip.setPreferredSize(d);
+				ip.setSize(d);
+				ip.setBorder(BorderFactory.createEtchedBorder());
+				ip.setBackground(Color.BLACK);
+				JPanel camPanel = new JPanel();
+				camPanel.add(ip, BorderLayout.NORTH);
+				camPanel.add(irsens, BorderLayout.SOUTH);
+				camd.add(camPanel);
+//				camd.add(ip);
+//				camd.add(irsens);
+				camd.setSize(350,380);
+//				camd.pack();
+				camd.setLocation(100,200);
+				camd.setVisible(true);
+				camd.setTitle("IRDot Monitor");
+				camd.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+				camd.setAlwaysOnTop(true);
+   }
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton buttonConnect;
     private javax.swing.JButton buttonMonitor;
@@ -249,6 +600,8 @@ public class MainUI extends javax.swing.JFrame {
     private javax.swing.JMenu jMenu3;
     private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JMenuItem jMenuItem1;
+    private javax.swing.JMenuItem jMenuItem2;
+    private javax.swing.JMenuItem jMenuItem3;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JPanel jPanel3;
